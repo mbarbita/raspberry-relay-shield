a60d214376eeae4ced9767ed4681bb9e301d072a
mbarbita <mbarbita@gmail.com>
2015-12-30 04:43:15 +0200

    Add comm browser-go func, css, toggle on click.

--- raspberry-relay-shield.go
+++ raspberry-relay-shield.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/davecheney/i2c"
+	//"github.com/davecheney/i2c"
 )
 
 // Default Request Handler
@@ -26,7 +26,7 @@ func post(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		body, _ := ioutil.ReadAll(r.Body)
 		log.Println("body", string(body))
-		log.Println("body", body)
+		//log.Println("body", body)
 		msg = string(body[3:])
 	case "GET":
 		r.ParseForm()
@@ -48,16 +48,21 @@ func post(w http.ResponseWriter, r *http.Request) {
 	log.Println("msg", msg)
 
 	switch msg {
-	case "128":
-		fmt.Fprintf(w, "%s", "relays status")
+	case "startup":
+		cha <- 0
+		fmt.Fprintf(w, "%v", <-chb)
 	case "r1":
-		fmt.Fprintf(w, "clicked %s!", msg)
+		cha <- 1
+		fmt.Fprintf(w, "%v", <-chb)
 	case "r2":
-		fmt.Fprintf(w, "clicked %s!", msg)
+		cha <- 2
+		fmt.Fprintf(w, "%v", <-chb)
 	case "r3":
-		fmt.Fprintf(w, "clicked %s!", msg)
+		cha <- 3
+		fmt.Fprintf(w, "%v", <-chb)
 	case "r4":
-		fmt.Fprintf(w, "clicked %s!", msg)
+		cha <- 4
+		fmt.Fprintf(w, "%v", <-chb)
 
 	default:
 		fmt.Fprintf(w, "%s", "invalid req")
@@ -67,10 +72,67 @@ func post(w http.ResponseWriter, r *http.Request) {
 }
 
 func raspberry() {
-	i, err := i2c.New(0x20, 1)
+	//i, err := i2c.New(0x20, 1)
 }
 
+// Sets the bit at pos in the integer n.
+func setBit(n int, pos uint) int {
+	n |= (1 << pos)
+	return n
+}
+
+// Clears the bit at pos in n.
+func clearBit(n int, pos uint) int {
+	mask := ^(1 << pos)
+	n &= mask
+	return n
+}
+
+func hasBit(n int, pos uint) bool {
+	val := n & (1 << pos)
+	return (val > 0)
+}
+
+var cha = make(chan int)
+var chb = make(chan int)
+
 func main() {
+
+	go func() {
+		rs := 0
+		for {
+			msga := <-cha
+			switch msga {
+			case 0:
+			case 1:
+				if hasBit(rs, 0) {
+					rs = clearBit(rs, 0)
+				} else {
+					rs = setBit(rs, 0)
+				}
+			case 2:
+				if hasBit(rs, 1) {
+					rs = clearBit(rs, 1)
+				} else {
+					rs = setBit(rs, 1)
+				}
+			case 3:
+				if hasBit(rs, 2) {
+					rs = clearBit(rs, 2)
+				} else {
+					rs = setBit(rs, 2)
+				}
+			case 4:
+				if hasBit(rs, 3) {
+					rs = clearBit(rs, 3)
+				} else {
+					rs = setBit(rs, 3)
+				}
+			}
+			chb <- rs
+			log.Printf("bitwise: %b", rs)
+		}
+	}()
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css-raspberry-relay-shield"))))
 	http.HandleFunc("/ajax/post.html", post)
 	http.HandleFunc("/", defaultHandler)
