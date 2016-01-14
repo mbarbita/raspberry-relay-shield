d8ca8a48bfa25d443363d861e9024aa2e07650d5
mbarbita <mbarbita@gmail.com>
2016-01-14 14:37:51 +0200

    Update relay.py

--- relay.py
+++ relay.py
@@ -5,4 +5,4 @@ import sys
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 data = int(sys.argv[1])
 print ~data
-#bus.write_byte_data(0x20, 0x06, ~data)
+bus.write_byte_data(0x20, 0x06, ~data)
